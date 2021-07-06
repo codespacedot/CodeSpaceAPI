d6a4afcf5824868e920d6c4adc443b58ef422d97
Vish <cloudmail.vishwajeet@gmail.com>
2021-07-06 13:10:59 +0530

    refactor: Change imports style

--- src/routes/academics.py
+++ src/routes/academics.py
@@ -1,9 +1,9 @@
 from fastapi import APIRouter, status
-from src.core.academics import *
+from src.core import academics as core
 
 router = APIRouter(prefix='/academics', tags=['Academics'])
 
 
 @router.get('/{year}', status_code=status.HTTP_200_OK)
 def data_for_year(year: int):
-    return get_data_for_year(year)
+    return core.get_data_for_year(year)
