fb94b2968b729b7a73bfa92cb54a9a6ccee136b6
Vish <cloudmail.vishwajeet@gmail.com>
2021-07-19 09:13:54 +0530

    feat: Timestamp based file name

--- src/utils/string_utils.py
+++ src/utils/string_utils.py
@@ -7,8 +7,10 @@ __date__ = '17/07/21'
 __email__ = 'cloudmail.vishwajeet@gmail.com'
 
 # Library Imports
+from datetime import datetime
 import string
 import random
+
 
 # Own Imports
 from .. import settings
@@ -25,3 +27,20 @@ def verification_code() -> str:
     code = ''.join([random.choice(data) for _ in range(settings.VERIFICATION_CODE_LENGTH)])
 
     return code
+
+
+def file_name(name: str, key: str) -> str:
+    """Generate file name.
+
+    Generate a random file name based on current UTC timestamp and specified key.
+
+    Arguments:
+    ---------
+        name: File name.
+        key: Key to add for randomness.
+
+    Returns:
+    ---------
+        Alphanumeric code of length 'src.settings.VERIFICATION_CODE_LENGTH'
+    """
+    return datetime.utcnow().strftime(f'%Y%m%d%H%M%S{key}') + '.' + name.split('.')[1]
